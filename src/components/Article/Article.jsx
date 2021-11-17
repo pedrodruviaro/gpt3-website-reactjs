492bcc86715a6eef07f60ed0a55b1ecdf62b6ef1
pedrodruviaro <pedrodruviaro@gmail.com>
2021-11-17 20:51:10 -0300

    blog section done

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import "./article.css";
 
-export const Article = () => {
-    return <div>Article</div>;
+export const Article = ({ image, date, title }) => {
+    return (
+        <div className="gpt3__blog-container_article">
+            <div className="gpt3__blog-container_article-image">
+                <img src={image} alt="blog post" />
+            </div>
+
+            <div className="gpt3__blog-container__article-content">
+                <div>
+                    <p>{date}</p>
+                    <h3>{title}</h3>
+                </div>
+                <p>Read Full Article</p>
+            </div>
+        </div>
+    );
 };
